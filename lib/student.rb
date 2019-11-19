697211f64fa7b2074605036285006feeeec220a1
Nicholas Ng <nicholasykng@gmail.com>
2019-11-19 04:44:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,7 +56,8 @@ class Student
 
     def self.find_by_name(name)
       sql = "SELECT * FROM students WHERE name = ?"
-      DB[:conn].execute(sql, name)[0]
+      result = DB[:conn].execute(sql, name)[0]
+      Student.new(result[0], result[1], result[2])
     end
 
 
