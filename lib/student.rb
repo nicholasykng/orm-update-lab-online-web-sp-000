422a06274242e7b88d691d61b8731d2f9599c16d
Nicholas Ng <nicholasykng@gmail.com>
2019-11-19 04:21:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,7 +38,7 @@ class Student
     end
     def update
       sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
-      DB[:conn].execute(sql, self.name, self.album, self.id)
+      DB[:conn].execute(sql, self.name, self.grade, self.id)
     end
 
 
