5a0c1312f20f3f7398ea851f507079a55793c73c
Nicholas Ng <nicholasykng@gmail.com>
2019-11-19 04:22:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,10 +36,19 @@ class Student
         @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
       end
     end
+
     def update
       sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
       DB[:conn].execute(sql, self.name, self.grade, self.id)
     end
+
+    def self.create(name: , grade: )
+      student = Student.new(name, grade)
+      student.save
+      student
+    end
+
+
 
 
 
